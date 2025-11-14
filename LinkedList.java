e5aa966cb30b8c12607bc87e0303ca4e06496fc4
2005pearls <janaalmengash@icloud.com>
2025-11-14 20:01:59 +0300

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,108 +1,81 @@
-package structurephase1;
+package draft;
 
 class Node<T> {
-    public T data;
-    public Node<T> next;
-    Object link;
-
-    public Node(T val) {
-        data = val;
-        next = null;      
-    }
+    T data;
+    Node<T> next;
+    Node(T d){ data = d; }
 }
 
-//LinkedList Class 
 public class LinkedList<T> {
+    private Node<T> head;
+    private Node<T> current;
 
-    private Node<T> head;      
-    private Node<T> current;   
+    public LinkedList(){ head = null; current = null; }
 
-    
-    public LinkedList() {
-        head = null;
+    public boolean empty(){ return head == null; }
+
+    public boolean last(){
+        return current != null && current.next == null;
+    }
+
+    public void findFirst(){
         current = head;
     }
 
-    
-    public boolean empty() { 
-        return head == null;
+    public void findNext(){
+        if (current != null) current = current.next;
     }
 
-  
-    public boolean last() {
-        return current.next==null;
+    public T retrieve(){
+        return (current == null) ? null : current.data;
     }
 
-
-    public boolean full() {
-        return false;
+    public void update(T val){
+        if (current != null) current.data = val;
     }
 
-    
-    public void findFirst() {
-        current = head;
+    public void insert(T val){
+        Node<T> nn = new Node<>(val);
+        if (empty()){
+            head = nn;
+            current = head;
+        }else{
+            nn.next = (current == null) ? head : current.next;
+            if (current == null){ 
+                head = nn;
+                current = head;
+            }else{
+                current.next = nn;
+                current = nn;
+            }
+        }
     }
 
-  
-    public void findNext() {
-      current=head;
-        
+    public void remove(){
+        if (empty() || current == null) return;
+
+        if (current == head){
+            head = head.next;
+            current = head;
+            return;
+        }
+        Node<T> p = head;
+        while (p != null && p.next != current) p = p.next;
+        if (p == null) return; 
+        p.next = current.next;
+        current = (p.next == null) ? head : p.next;
     }
 
-    
-    public T retrieve() {
-      return current.data;
-    }
-    
-    public void update (T val){
-        current.data=val;
-    }
-
-   
-    public void insert(T val) {
-    Node<T> tmp;
-    if (empty()) {
-        current = head = new Node<T>(val);
-    } else {
-        tmp = current.next;
-        current.next = new Node<T>(val);
-        current = current.next;
-        current.next = tmp;
+    public void addLast(T val){
+        Node<T> nn = new Node<>(val);
+        if (empty()){
+            head = nn;
+            current = head;
+            return;
+        }
+        Node<T> p = head;
+        while (p.next != null) p = p.next;
+        p.next = nn;
+        current = nn;
     }
 }
-
-    public void remove() {
-    if (current == head) {
-        head = head.next;
-    } else {
-        Node<T> tmp = head;
-
-        while (tmp.next != current)
-            tmp = tmp.next;
-
-        tmp.next = current.next;
-    }
-
-    if (current.next == null)
-        current = head;
-    else
-        current = current.next;
-}
-    
-// add new node at the end of the list
-public void addLast(T x) {
-    Node<T> nn = new Node<>(x);
-    if (head == null) {
-        head = nn;
-        current = head;
-        return;
-    }
-    Node<T> n = head;
-    while (n.next != null) n = n.next;
-
-    n.next = nn;
-    current = nn;
-}
-
-}//end class
-
