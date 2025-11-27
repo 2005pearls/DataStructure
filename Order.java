dc542469b556dccc4ba4b73645fc38d3ebdfec53
wasanalamri-20 <wasanalamri456654@gmail.com>
2025-11-27 21:13:51 +0300

    Update Order.java

--- Order.java
+++ Order.java
@@ -6,11 +6,19 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Order {
-
+private int orderID;
+    private Customer customer;                     // reference
+     private LinkedList<OrderItem> items = new LinkedList<>();
+     private double totalPrice;
+     private String orderDate;                      // yyyy-MM-dd
+     private String status;                         // pending/shipped/delivered/canceled
+    
+     private static BST<Order> ordersTree = new BST<>();
+     
+     
     private static class OrderItem {
         Product product;
         int qty;
-        
         OrderItem(Product p, int q) { 
             product = p; qty = q; }
         
@@ -22,17 +30,8 @@ public class Order {
             System.out.println(" - " + product.getName() + " (x" + qty + ")  price=" + product.getPrice() +"  lineTotal=" + String.format("%.2f", lineTotal()));
         }
     }
-
     
-    
-    private int orderID;
-    private Customer customer;                     // reference
-     private LinkedList<OrderItem> items = new LinkedList<>();
-     private double totalPrice;
-     private String orderDate;                      // yyyy-MM-dd
-     private String status;                         // pending/shipped/delivered/canceled
 
-    private static LinkedList<Order> allOrders = new LinkedList<>();
 // to format the date and make it acceptable
     private static final DateTimeFormatter DF = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 // create with device date
@@ -60,48 +59,38 @@ public class Order {
     public double getTotalPrice(){ return totalPrice; }
     public Customer getCustomer(){ return customer; }
 
-    //  add obj at the end of the list 
-    private static void insertAtEnd(Order o) {
-        if (allOrders.empty()) allOrders.insert(o);
-        else {
-            allOrders.findFirst();
-            while (!allOrders.last()) allOrders.findNext();
-            allOrders.insert(o);
-        }
-    }
+   
 
-    //  search / create / cancel / update 
-    public static Order searchById(int id) {
-        if (allOrders.empty()) 
+     public static Order searchById(int id) {
+        if (!ordersTree.findKey(id))
             return null;
-        allOrders.findFirst();
-        while (true) {
-            Order cur = allOrders.retrieve();
-            if (cur.orderID == id) return cur;  // current stops here
-            if (allOrders.last()) break;
-            allOrders.findNext();
-        }
-        return null;
+        return ordersTree.retrieve();
     }
-
+     
+     private static void registerOrder(Order o) {
+        int id = o.getOrderID();
+        if (ordersTree.findKey(id)) {
+            return;
+        }
+        ordersTree.insert(id, o);
+    }
+     
     //Create order and register 
     public static Order create(int orderID, int customerID, String status) {
         if (searchById(orderID) != null) {
             System.out.println("Order " + orderID + " already exists!");
             return null;
         }
-        
+
         Customer c = Customer.searchById(customerID);
         if (c == null) {
             System.out.println("Customer " + customerID + " not found.");
             return null;
-            
         }
-        
         Order o = new Order(orderID, c, status);
-        insertAtEnd(o);
+        registerOrder(o);
         c.placeOrder(o);
-        
+
         return o;
     }
 
@@ -114,14 +103,14 @@ public class Order {
         if (!items.empty()) {
             items.findFirst();
             while (true) {
-               OrderItem it = items.retrieve();
-              it.product.setStock(it.product.getStock() + it.qty);
-              if (items.last()) break;
-               items.findNext();
+                OrderItem it = items.retrieve();
+                it.product.setStock(it.product.getStock() + it.qty);
+                if (items.last()) break;
+                items.findNext();
             }
         }
         status = "canceled";
-       System.out.println("Order " + orderID + " canceled.");
+        System.out.println("Order " + orderID + " canceled.");
     }
 
     public void updateStatus(String newStatus) {
@@ -137,17 +126,17 @@ public class Order {
             System.out.println("qty must be > 0");
             return false;
         }
-       Product p = Product.searchById(productId);
-      if (p == null) {
-           System.out.println("Product " + productId + " not found.");
-           return false;
+
+        Product p = Product.searchById(productId);
+        if (p == null) {
+            System.out.println("Product " + productId + " not found.");
+            return false;
         }
         if (p.getStock() < qty) {
-           System.out.println("Not enough stock for " + p.getName());
-          return false;
+            System.out.println("Not enough stock for " + p.getName());
+            return false;
         }
 
-        // append item 
         OrderItem it = new OrderItem(p, qty);
         if (items.empty()) items.insert(it);
         else {
@@ -156,25 +145,24 @@ public class Order {
             items.insert(it);
         }
 
-        // update totals
         totalPrice += it.lineTotal();
         p.setStock(p.getStock() - qty);
         return true;
     }
 
+
+
     //  display 
     public void display() {
         System.out.println("Order ID: " + orderID +
-               " | date: " + orderDate +
+                " | date: " + orderDate +
                 " | status: " + status +
-               " | customer: " + (customer != null ? customer.getName() : "null") +
+                " | customer: " + (customer != null ? customer.getName() : "null") +
                 " | total: " + String.format("%.2f", totalPrice));
+
         if (items.empty()) {
             System.out.println("  (no items)");
-      
-        }else
-            
-        {
+        } else {
             items.findFirst();
             while (true) {
                 items.retrieve().display();
@@ -185,85 +173,95 @@ public class Order {
         System.out.println("------------------------------------");
     }
 
-    //  views and  queries 
     
     //Show all orders of one customer 
     public static void viewOrdersOfCustomer(int customerID) {
         Customer c = Customer.searchById(customerID);
-        if (c == null) { System.out.println("Customer not found."); return; }
-
-        System.out.println("\n== Orders for " + c.getName() + " ==");
-        boolean any = false;
-        if (!allOrders.empty()) {
-            allOrders.findFirst();
-            while (true) {
-                Order o = allOrders.retrieve();
-                
-                if (o.customer != null && o.customer.getCustomerID() == customerID) {
-                    o.display();
-                    any = true;
-                }
-                
-                if (allOrders.last()) break;
-                allOrders.findNext();
-            }
+        if (c == null) {
+            System.out.println("Customer not found.");
+            return;
         }
-     if (!any) System.out.println("(no orders)");
+        c.viewOrders();
     }
 
-    // print all orders between two date in this yyyy-MM-dd */
-   public static void ordersBetween(String fromYmd, String toYmd) {
-        fromYmd = formatDate(fromYmd);
-        toYmd = formatDate(toYmd);
+    //  1. Find All Orders Between Two Dates (use in-order traversal). yyyy-MM-dd */
+  public static void ordersBetween(String fromYmd, String toYmd) {
+    fromYmd = formatDate(fromYmd);
+    toYmd = formatDate(toYmd);
 
-        try {
-            LocalDate a = LocalDate.parse(fromYmd, DF);
-            LocalDate b = LocalDate.parse(toYmd, DF);
-
-            if (a.isAfter(b)) {
-                LocalDate t = a;
-                a = b;
-                b = t;
-            }
-
-            System.out.println("\n== All orders ==");
-
-            boolean any = false;
-
-            if (!allOrders.empty()) {
-                allOrders.findFirst();
-                while (true) {
-                    Order o = allOrders.retrieve();
-                    String[] orderDetails = o.orderDate.split(",");  
-                    LocalDate d = LocalDate.parse(orderDetails[1], DF);  
-
-                    if ((!d.isBefore(a)) && (!d.isAfter(b))) {
-                        o.display();  
-                        any = true;
-                    }
-                    if (allOrders.last()) break;
-                    allOrders.findNext();
-                }
-            }
-
-        if (!any) System.out.println("(none)");
-        } catch (Exception e) {
-            System.out.println("Error parsing dates: " + e.getMessage());
+    try {
+        LocalDate a = LocalDate.parse(fromYmd, DF);
+        LocalDate b = LocalDate.parse(toYmd, DF);
+        
+       
+      
+        if (a.isAfter(b)) {
+          LocalDate t = a;
+            a = b;
+           b = t;
         }
-    }
-// method make suer fo date format 
-    private static String formatDate(String date) {
-        if (date.matches("\\d{4}/\\d{1}/\\d{1}")) {
-            return date.replaceAll("(\\d{4})/(\\d{1})/(\\d{1})", "$1-0$2-0$3"); 
-        } else if (date.matches("\\d{4}-\\d{1}-\\d{1}")) {
-            return date.replaceAll("(\\d{4})-(\\d{1})-(\\d{1})", "$1-0$2-0$3"); 
+       System.out.println ("\n== Orders between " + a + " and " + b + " ==");
+
+        if (ordersTree.isEmpty()) {
+           System.out.println("(none)");
+            return;
         }
-        return date;  
+        
+
+       boolean any =  ordersBetweenRec(ordersTree.getRoot(), a, b);
+        if (!any) {
+            System.out.println("(none)");
+       }
+
+        
+    } catch  (Exception e) {
+          System.out.println("Error parsing dates: " + e.getMessage());
+   }
+}
+
+
+private static boolean ordersBetweenRec(BSTNode<Order> p,  LocalDate a, LocalDate b) {
+    
+    if (p == null) return false;
+    boolean any = false;
+    any |= ordersBetweenRec(p.left, a, b);
+    Order o = p.data;
+    try {
+        String dateOnly = o.orderDate.contains(",")
+               ? o.orderDate.split(",")[1].trim()
+               : o.orderDate.trim();
+        DateTimeFormatter ff = DateTimeFormatter.ofPattern("M/d/yyyy");
+         LocalDate d = LocalDate.parse(dateOnly, ff);
+        if (!d.isBefore(a) && !d.isAfter(b)) {
+             o.display();
+             any = true;
+        }
+    } catch  (Exception ex) {
+          System.out.println("Bad date format for order " + o.orderID + ": " + o.orderDate);
     }
+    any |=  ordersBetweenRec(p.right , a, b);
+    return  any;
+} 
+
+
+
+
+
+
+// method make sure for date format 
+private static String formatDate(String  date) {
+    if (date.matches("\\d{4}/\\d{1}/\\d{1}")) {
+        return date.replaceAll("(\\d{4})/(\\d{1})/(\\d{1})", "$1-0$2-0$3"); 
+    } else if (date.matches("\\d{4}-\\d{1}-\\d{1}")) {
+        return date.replaceAll("(\\d{4})-(\\d{1})-(\\d{1})", "$1-0$2-0$3"); 
+    }
+    return date;  
+}
+
 
 
     //  CSV load that includes orderId and customerId and status and yyyy-MM-dd of date 
-    public static void load_orders(String fileName) {
+    public static void load_orders(String  fileName) {
         try (Scanner sc = new Scanner(new File(fileName), "UTF-8")) {
             if (sc.hasNextLine()) sc.nextLine(); // skip header
             int cnt = 0;
@@ -274,8 +272,8 @@ public class Order {
 
                String[] a = line.split(",", 4);
                int oid = Integer.parseInt(a[0].trim());
-                int cid = Integer.parseInt(a[1].trim());
-               String status = a[2].trim();
+              int cid = Integer.parseInt(a[1].trim());
+             String status = a[2].trim();
                String date = a[3].trim();
 
                if (searchById(oid) != null) continue;
@@ -286,12 +284,13 @@ public class Order {
                    continue;
                }
                 Order o = new Order(oid, c, status, date);
-               insertAtEnd(o);
-                c.placeOrder(o);
+               registerOrder(o);
+               c.placeOrder(o);
                 cnt++;
             }
         } catch (Exception e) {
             System.out.println("✗ Error reading orders: " + e.getMessage());
         }
     }
+    
 }
